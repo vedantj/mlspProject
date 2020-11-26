0e55d3afcc839e86710a475d5596f3945dd7bdb6
Vedant Jain <vedant@Vedants-MacBook-Pro-2.local>
2020-11-26 10:29:30 -0800

    new Updates

---
+++ getHForNeuron.m
@@ -0,0 +1,21 @@
+function Hlocs = getHForNeuron(H,W)
+%This function returns an array giving the H for that neuron
+%Array of 0's and 1's. Is N X K with N = number of neurons, K = length of time, 1's mean
+%spike, 0's mean no spike
+Hlocs = zeros(size(H));
+for i = 1:size(H,1)
+    currentH = H(i,:);
+    thres = prctile(currentH,99);
+    [pks loc] = findpeaks(currentH,'MinPeakProminence',thres);
+    Hlocs(i,loc) = 1;%pks
+    
+end
+%% plot the H's
+% figure();
+% plot(1:size(Hlocs,2), bsxfun(@plus, Hlocs, (abs((0:(size(Hlocs,1)-1))-(size(Hlocs,1)-1))')));
+% title('Time Series of Spikes for different Neurons');
+% ylabel('Neuron');
+% xlabel('Time');
+
+end
+
---
+++ mainPipeline.m
@@ -0,0 +1,56 @@
+%% main pipeline to output H's as spikes:
+% have X already selected
+%apply seqNMF
+K = 7;
+L = 20;%6000; %50 % length of timebins for each factor
+lambda =0;%.005;%0.005;
+lambdal1H = .48; % L1H just scales the HH
+lambdaorthoW = 0;%.0002;%.0005; %how orthogonal the W's have to be
+lOH = .1724; % lambda ortho 
+lW1 = .03; % scales the lambda of W
+shg; clf
+display('Running seqNMF on simulated data (2 simulated sequences + noise)')
+[W,H] = seqNMFVD(X,'K',K, 'L', L,'lambda', lambda,'lambdaL1H',lambdal1H,'lambdaL1W',lW1,'lambdaOrthoW',lambdaorthoW,'lambdaOrthoH',lOH);
+%% check P value for current configuration for NMF:
+values = [];
+blah = linspace(0,1,30);
+for i = 1:30
+    for j = 1:30
+    lOH = blah(i);
+    lambdal1H = blah(j)
+    [W,H] = seqNMFVD(X,'K',K, 'L', L,'lambda', lambda,'lambdaL1H',lambdal1H,'lambdaL1W',lW1,'lambdaOrthoW',lambdaorthoW,'lambdaOrthoH',lOH,'showplot',0);
+    Hlocs = getHForNeuron(H,W);
+    X_hat = helper.reconstruct(W,Hlocs);
+    t = norm(vecnorm((X*X_hat')'),2)/sum(Hlocs,'all');
+    values(i,j) = t;
+    disp(i)
+    disp(j)
+
+
+    end
+end
+
+%% Look at factors
+figure; SimpleWHPlot(W,H); title('SeqNMF reconstruction')
+figure; SimpleWHPlot(W,H,X); title('SeqNMF factors, with raw data')
+
+%% get H's
+Hlocs = getHForNeuron(H,W);
+X_hat = helper.reconstruct(W,Hlocs);
+res = X - X_hat;
+figure();
+plot(1:size(X_hat,2), bsxfun(@plus, X_hat, (abs((0:(size(X_hat,1)-1))-(size(res,1)-1))')));
+figure();
+plot(1:size(X,2), bsxfun(@plus, X, (abs((0:(size(X,1)-1))-(size(X,1)-1))')));
+t = norm(vecnorm((X*X_hat')'),2)/sum(Hlocs,'all');
+%% get h's
+figure();
+plot(Hlocs');
+ylim([0 1.5]);
+colSum = sum(Hlocs);
+colSum(colSum > 0) = 1;
+
+%% find distance
+
+
+[ro co] = find(values == max(values,[],'all'));
